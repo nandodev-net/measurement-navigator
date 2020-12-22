e86a00c455d4160d8f1046ee3dc02e80301adf18
Gibsy <gibyvngr@gmail.com>
2020-12-22 12:15:48 -0400

    fixed dates format issue

--- vsf/apps/api/fp_tables_api/tasks.py
+++ vsf/apps/api/fp_tables_api/tasks.py
@@ -29,6 +29,7 @@ def fp_update(since : str = None, until : str = None, only_fastpath : bool = Fal
     state = cache.get("update-fastpath")
     if state == ProcessState.RUNNING:
         return
+    date_format = "%Y-%m-%d"
     # note that we use "now" as tomorrow, because the request truncates 
     # the "time" part of the datetime, so the ooni query will search
     # until the next day at the 00:00:00 hour, including 'today' in the query,
@@ -38,12 +39,13 @@ def fp_update(since : str = None, until : str = None, only_fastpath : bool = Fal
     cache.set("update-fastpath", ProcessState.RUNNING)
     if until is None:
         until = datetime.now() + timedelta(days=1)
+        until = datetime.strftime(until, date_format)
 
     if since is None:
-        since = until - timedelta(days=1)
-
-    until = datetime.strftime(until,"%Y-%m-%d")
-    since = datetime.strftime(since,"%Y-%m-%d")
+        until_datetime = datetime.strptime(until, date_format)
+        since = until_datetime - timedelta(days=1)
+        since = datetime.strftime(since, date_format)
+    
     try: 
         request_fp_data(since, until, only_fastpath)
         cache.set("update-fastpath", ProcessState.IDLE)
