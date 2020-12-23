5fa50f985217eec6978a41a5502fbae22ef002e7
Fernando Gonzalez <gonzalez0fer@gmail.com>
2020-12-23 16:20:03 -0400

    select debugged

--- vsf/help_scripts/select_test.py
+++ vsf/help_scripts/select_test.py
@@ -7,18 +7,39 @@ submeasurements = [DNS, TCP, HTTP]
 for SM in submeasurements:
 
     # Get the data we need, all the submeasurements ordered by input, and by date
-    sms = SM.objects.all()\
-            .select_related('measurement', 'measurement__raw_measurement', 'flag')\
-            .exclude(flag=None)\
-            .order_by('measurement__raw_measurement__input', 'measurement__raw_measurement__measurement_start_time')\
-            .values('measurement__raw_measurement__input', 'id', 'flag__flag')
-    groups = Grouper(sms) 
+    submeasurements = [(HTTP,'http'), (TCP,'tcp'), (DNS, 'dns')]
 
-    for group in groups:
-        start_time = time()
-        result = select(group)
-        elapsed_time = time() - start_time
+    # just a shortcut
+    start_time = lambda m : m.measurement.raw_measurement.measurement_start_time
 
-        print(elapsed_time)
-        print(result)
-        break
+    result = {'hard_tagged':[]}
+    # For every submeasurement type...
+    for (SM, label) in submeasurements:
+
+        meas = SM.objects.raw(
+            (   "SELECT " +
+                "submeasurements_{label}.id, " +
+                "previous_counter, " +
+                "rms.measurement_start_time, " +
+                "dense_rank() OVER (order by rms.input) as group_id " +
+
+                "FROM " +
+                "submeasurements_{label} JOIN measurements_measurement ms ON ms.id = submeasurements_{label}.measurement_id " +
+                                        "JOIN measurements_rawmeasurement rms ON rms.id = ms.raw_measurement_id " +
+                                        "JOIN flags_flag f ON f.id = submeasurements_{label}.flag_id " +
+                "WHERE " +
+                "f.flag<>'ok' " +
+                "ORDER BY rms.input, rms.measurement_start_time, previous_counter; "
+            ).format(label=label)
+        )
+        
+        groups = Grouper(meas, lambda m: m.group_id)
+
+        for group in groups:
+            start_time = time()
+            result = select(group)
+            elapsed_time = time() - start_time
+
+            print(elapsed_time)
+            print(result)
+            
