2335e6bd76dca7d5fe4b925d397ae90531695526
Gibsy <gibyvngr@gmail.com>
2021-01-22 11:10:22 -0400

    updated query to count measurements to be more performant

--- vsf/sql/update_previous_counter_dns.sql
+++ vsf/sql/update_previous_counter_dns.sql
@@ -1,15 +1,36 @@
 -- This query can update the previous_counter for dns submeasurements
-EXPLAIN analyze WITH sq as (
-    SELECT 
+WITH 
+    measurements as (
+        SELECT 
             dns.id as dns_id, 
-            sum(CAST(f.flag='soft' AS INT)) OVER (PARTITION BY rms.input ORDER BY rms.measurement_start_time, dns.id ASC) as previous
-    FROM
-        submeasurements_dns dns JOIN measurements_measurement ms ON ms.id = dns.measurement_id
-                                JOIN measurements_rawmeasurement rms ON rms.id = ms.raw_measurement_id
-                                JOIN flags_flag f ON f.id = dns.flag_id
+            rms.measurement_start_time as start_time,
+            f.flag as flag,
+            rms.input as input,
+            dns.counted as counted,
+            dns.previous_counter as prev_counter
+        FROM submeasurements_dns dns    JOIN measurements_measurement ms ON ms.id = dns.measurement_id
+                                        JOIN measurements_rawmeasurement rms ON rms.id = ms.raw_measurement_id
+                                        JOIN flags_flag f ON f.id = dns.flag_id
+        ORDER BY input, start_time, prev_counter, dns_id
+    ),
+    ms_to_update as (
+        SELECT DISTINCT ms.input 
+        FROM measurements ms 
+        WHERE NOT ms.counted
+    ),
+    sq as (
+        SELECT 
+            ms.dns_id as id, 
+            ms.input  as input, 
+            ms.flag   as flag, 
+            SUM(CAST(ms.flag<>'ok' AS INT)) OVER (PARTITION BY ms.input ORDER BY ms.start_time, ms.prev_counter, ms.dns_id) as prev_counter
+        FROM measurements ms JOIN ms_to_update on ms_to_update.input = ms.input
     )
+-- SELECT * FROM sq;
+
 UPDATE submeasurements_dns dns
     SET 
-        previous_counter = sq.previous
+        previous_counter = sq.prev_counter,
+        counted = CAST(1 as BOOLEAN)
     FROM sq
-    WHERE dns.id = sq.dns_id;
+    WHERE dns.id = sq.id;
