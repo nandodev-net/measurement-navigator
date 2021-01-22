8bfaf66fd665e6a773ac8ea76f9683bf3e0c1a38
Fernando Gonzalez <gonzalez0fer@gmail.com>
2021-01-22 03:37:02 -0400

    list cases and detail case services

--- vsf/apps/api/urls.py
+++ vsf/apps/api/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
         'ooni_fp/',
         include('apps.api.fp_tables_api.urls', namespace='fp_tables'),
         ),
+
+    path(
+        'cases/',
+        include('apps.api.cases_api.urls', namespace='cases'),
+        ),
+
         path(
         'api-token-auth/', 
         obtain_auth_token, name='api_token_auth'),
