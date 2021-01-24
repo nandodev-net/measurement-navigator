cd141a91f07402d067fe13c36d635eab025bcdb6
jhondoes <jhondoes.does@gmail.com>
2021-01-24 03:11:26 -0400

    services: num active cases, num active events, num active events by ASN, num active events by type

--- vsf/apps/api/events_api/urls.py
+++ vsf/apps/api/events_api/urls.py
@@ -16,4 +16,18 @@ urlpatterns = [
         views.EventDetail.as_view(), 
         name='event_detail'),
 
+    path(
+        'num-active', 
+        views.EventActiveNumber.as_view(), 
+        name='event_active_num'),
+
+    path(
+        'num-asn', 
+        views.EventAsnNumber.as_view(), 
+        name='event_asn_num'),
+
+    path(
+        'num-type', 
+        views.EventTypeNumber.as_view(), 
+        name='event_type_num'),
 ]
