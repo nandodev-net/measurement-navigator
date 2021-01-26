e69d07530ddad5e5e52e81744390fab57d8c137e
Gibsy <gibyvngr@gmail.com>
2021-01-26 14:34:53 -0400

    initial version of hard flag optimization

--- vsf/help_scripts/assign_domain.py
+++ vsf/help_scripts/assign_domain.py
@@ -5,10 +5,17 @@ from vsf.utils                     import get_domain
 
 
 qs = Measurement.objects.all().select_related('raw_measurement').exclude(raw_measurement__input=None).filter(domain=None)
-updated = []
-for q in qs:
-    
+to_update = []
+count = 0
+for q in qs.iterator():
     domain, _ = Domain.objects.get_or_create(domain_name = get_domain(q.raw_measurement.input), defaults={'site':None})
     q.domain = domain
-    q.save()
-    del q
+    to_update.append(q)
+    if to_update == 10000:
+        count += len(to_update)
+        print(f"Updated {count} measurements so far")
+        Measurement.objects.bulk_update(to_update, ['domain'])
+        to_update = []
+
+if len(to_update) != 0: Measurement.objects.bulk_update(to_update, ['domain'])
+
