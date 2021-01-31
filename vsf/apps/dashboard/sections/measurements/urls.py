873288ddf591a14100cfeab3369d3e3af99d5050
patricia <patriciamacuto@gmail.com>
2021-01-31 01:47:36 -0400

    deleting the url that was made to be requested in ajax and hence, fill the datatable -we no longer need this-

--- vsf/apps/dashboard/sections/measurements/urls.py
+++ vsf/apps/dashboard/sections/measurements/urls.py
@@ -1,16 +1,10 @@
 from django.conf.urls import include
 from django.urls import path
-from .views import ListMeasurementsTemplate, ListMeasurementsBackEnd, MeasurementDetails, MeasurementDetailView
+from .views import ListMeasurementsTemplate, MeasurementDetails, MeasurementDetailView
 
 app_name = 'measurements'
 
 urlpatterns = [
-
-    path(
-        'measurements_data/',
-        ListMeasurementsBackEnd.as_view(),
-        name="list_measurements_data"
-    ),
 
     path(
         'list_measurements/',                    
