a0c3a60fa9d55ae3775a7ae2c215489cb1f1db96
Gibsy <gibyvngr@gmail.com>
2021-02-01 17:15:17 -0400

    optimizing measuremment list view

--- vsf/apps/main/measurements/submeasurements/models.py
+++ vsf/apps/main/measurements/submeasurements/models.py
@@ -15,7 +15,8 @@ class SubMeasurement(models.Model):
     """
     measurement = models.ForeignKey(
                                     to=Measurement,
-                                    on_delete=models.CASCADE)
+                                    on_delete=models.CASCADE,
+                                    db_index=True)
     flag = models.ForeignKey(to=Flag, null=True, on_delete=models.SET_NULL)
 
     # The following fields are required for the hard flag logic:
