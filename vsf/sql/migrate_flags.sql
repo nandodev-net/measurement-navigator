3659db71fec71fdfbd4923c30d67ff37723ba160
Gibsy <gibyvngr@gmail.com>
2021-02-04 11:35:12 -0400

    added flag rework to measurements views; added some helper scripts to migrate from previous flags schemen to our current one

--- vsf/sql/migrate_flags.sql
+++ vsf/sql/migrate_flags.sql
@@ -1,23 +1,27 @@
-UPDATE submeasurements_dns 
+UPDATE submeasurements_dns m
 SET
     flag_type=f.flag,
     event_id=f.event_id,
     confirmed=f.confirmed
 FROM
-    submeasurements_dns subm JOIN flags_flag f ON subm.flag_id=f.id;
+    submeasurements_dns subm JOIN flags_flag f ON subm.flag_id=f.id
+WHERE m.id = subm.id;
 
-UPDATE submeasurements_http 
+UPDATE submeasurements_http m
 SET
     flag_type=f.flag,
     event_id=f.event_id,
     confirmed=f.confirmed
 FROM
-    submeasurements_http subm JOIN flags_flag f ON subm.flag_id=f.id;
+    submeasurements_http subm JOIN flags_flag f ON subm.flag_id=f.id
+    
+WHERE m.id=subm.id;
 
-UPDATE submeasurements_tcp 
+UPDATE submeasurements_tcp m
 SET
     flag_type=f.flag,
     event_id=f.event_id,
     confirmed=f.confirmed
 FROM
-    submeasurements_tcp subm JOIN flags_flag f ON subm.flag_id=f.id;
+    submeasurements_tcp subm JOIN flags_flag f ON subm.flag_id=f.id
+where m.id=subm.id;
