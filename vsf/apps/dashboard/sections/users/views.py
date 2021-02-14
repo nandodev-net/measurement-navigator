8c57348c561ab6a47c9ae1ecdb4c614564a8a079
Fernando Gonzalez <gonzalez0fer@gmail.com>
2021-02-14 16:41:20 -0400

    user edit partial view

--- vsf/apps/dashboard/sections/users/views.py
+++ vsf/apps/dashboard/sections/users/views.py
@@ -1,5 +1,6 @@
 # Django imports
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic           import TemplateView
 from django.views.generic.edit import UpdateView, CreateView
 from django.contrib.auth.hashers import make_password
@@ -39,7 +40,7 @@ class UsersList(VSFLoginRequiredMixin, TemplateView):
             if i.is_superuser:
                 role ='Superuser'
                 
-            context['object_list'].append({'username':i.username, 'first_name':i.first_name, 
+            context['object_list'].append({'id':i.id,'username':i.username, 'first_name':i.first_name, 
             'last_name':i.last_name, 'email':i.email,'role':role, 'is_active':i.is_active })
         return context  
 
@@ -111,5 +112,84 @@ class UserCreateView(VSFLoginRequiredMixin, CreateView):
         return self.render_to_response(
             self.get_context_data(
                 form=form,
+            )
+        )
+
+
+class UserUpdateView(VSFLoginRequiredMixin, UpdateView):
+    form_class = CustomUserForm
+    model = CustomUser 
+    queryset = CustomUser.objects.all()
+    template_name = "registration/user-edit.html"
+    success_url = "/dashboard/users/"
+
+    def get_context_data(self, **kwargs):
+
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+
+        i = get_object_or_404(CustomUser, pk=self.kwargs['pk'])
+
+        context['object_list'] = []
+
+        if i.is_admin:
+            role = '2'
+        elif i.is_analist:
+            role = '3'
+        else:
+            role = '4'
+
+        if i.is_superuser:
+            role ='1'
+            
+        context['object_list'].append({'role':role})
+
+        return context  
+
+
+    def post(self, request, *args, **kwargs):
+
+        self.object = self.get_object()
+        form = self.get_form()
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+
+        self.object.username = form.cleaned_data['username']
+        self.object.first_name = form.cleaned_data['first_name']
+        self.object.last_name = form.cleaned_data['last_name']
+        self.object.email = form.cleaned_data['email']   
+
+        if form.cleaned_data['role'] == "2":
+            self.object.is_admin = True
+            self.object.is_staff = True
+
+        elif form.cleaned_data['role'] == "3":
+            self.object.is_analist = True
+            self.object.is_staff = True
+        
+        else:
+            self.object.is_editor = True
+            self.object.is_staff = True
+
+
+        if form.cleaned_data['role'] == "1":
+            self.object.is_admin = True
+            self.object.is_analist = True
+            self.object.is_editor = True
+            self.object.is_staff = True
+            self.object.is_superuser = True
+        
+        self.object.save()
+        return HttpResponseRedirect("/dashboard/users/")
+
+
+    def form_invalid(self, form):
+        return self.render_to_response(
+            self.get_context_data(
+                form=form
             )
         )
