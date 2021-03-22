18be474d4d074a68b1df3bc8248b56801ddbdc29
gipsy-avn <gibyvngr@gmail.com>
2021-03-22 16:06:38 -0400

    including config information

--- vsf/help_scripts/testing_hard_flag_performance.py
+++ vsf/help_scripts/testing_hard_flag_performance.py
@@ -1,8 +1,22 @@
 from apps.main.measurements.submeasurements.FlagsUtils import hard_flag, count_flags_sql
-from datetime import datetime
+from apps.configs.models    import Config
+from datetime import datetime, timedelta
+
+
+config = Config.objects.all().first()
+
+if config:
+    interval = config.hardflag_timewindow
+    min_meas = config.hardflag_minmeasurements
+    interval_size = config.hardflag_interval_size
+else:
+    interval = timedelta(days=1)
+    min_meas = 7
+    interval_size = 10
+
 
 count_flags_sql()
 start = datetime.now()
-hard_flag()
+hard_flag(interval, min_meas, interval_size)
 end = datetime.now()
 print((end - start).total_seconds())
