08b1c0b8bef6d3b61892eab59ac6bc59346ee460
jhondoes <jhondoes.does@gmail.com>
2021-06-25 21:21:57 -0400

    tz fixes

--- vsf/apps/dashboard/utils.py
+++ vsf/apps/dashboard/utils.py
@@ -1,10 +1,10 @@
 import pytz
+from datetime import datetime, timedelta
 
 CARACAS = pytz.timezone('America/Caracas')
 
 def utc_aware_date(date, sys_tz):
     aware_date = date
-    print(date)
-    if sys_tz == 2:
-        aware_date = date.replace(tzinfo=CARACAS)
-    return aware_date.astimezone(pytz.UTC)
+    if sys_tz == "2":
+        aware_date = date - timedelta(hours=4, minutes=28)
+    return aware_date.astimezone()
