12b97613f33aaeddc894bcc668b9c8e760c51b19
jhondoes <jhondoes.does@gmail.com>
2021-12-11 04:08:24 -0400

    S3 final ingest prototype

--- vsf/apps/api/fp_tables_api/sync_measurements.py
+++ vsf/apps/api/fp_tables_api/sync_measurements.py
@@ -1,100 +1,49 @@
 from django.conf        import settings
 import datetime as dt
-from functools import singledispatch
-import gzip
-import logging
-from multiprocessing.pool import ThreadPool
-import os
-import pathlib
-from typing import List
-import ujson
-
-from . import ooni_client
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
+from urllib.parse import SplitResult
+from pathlib import PosixPath
+import posixpath
 
 
-@singledispatch
-def trim_measurement(json_obj, max_string_size: int):
-    return json_obj
+_BUCKET = 'ooni-data-eu-fra'
+_PREFIX = 'raw/'
 
 
-@trim_measurement.register(dict)
-def _(json_dict: dict, max_string_size: int):
-    keys_to_delete: List[str] = []
-    for key, value in json_dict.items():
-        if type(value) == str and len(value) > max_string_size:
-            keys_to_delete.append(key)
-        else:
-            trim_measurement(value, max_string_size)
-    for key in keys_to_delete:
-        del json_dict[key]
-    return json_dict
-
-
-@trim_measurement.register(list)
-def _(json_list: list, max_string_size: int):
-    for item in json_list:
-        trim_measurement(item, max_string_size)
-    return json_list
-
-
-class CostLimitError(Exception):
-    def __init__(self, message: str) -> None:
-        super().__init__(message)
-
-def _make_local_path(output_dir: pathlib.Path, entry: ooni_client.FileEntry) -> pathlib.Path:
-    basename = pathlib.PurePosixPath(entry.url.path).name
-    # Convert .json.lz4 and .tar.lz4 filenames.
-    if not basename.endswith('.jsonl.gz'):
-        basename = basename.rsplit(settings.MEDIA_ROOT, 2)[0] + '.jsonl.gz'
-    return output_dir / basename
-
-
-def s3_measurements_download(test_type=None, country:str='VE', 
+def s3_measurements_download(test_type:str='webconnectivity', country:str='VE', 
     first_date:str=(dt.date.today() - dt.timedelta(days=3)), 
-    last_date:str=(dt.date.today() - dt.timedelta(days=2)), max_string_size:int=1000, 
-    cost_limit_usd:float=100.00):
+    last_date:str=(dt.date.today() - dt.timedelta(days=2)),
+    output_dir:str='./media/ooni_data/'):
 
-    print('Since: ', first_date, ' to: ', last_date)
-    logging.basicConfig(level=logging.INFO)
+    print('Since: ', first_date, ' at 00:00 To: ', last_date,' at 00:00')
 
-    logging.basicConfig(level=logging.DEBUG)
+    conn = boto3.client(
+                's3', config=Config(signature_version=UNSIGNED))
 
-    ooni = ooni_client.OoniClient()
-    num_measurements = 0
-    file_entries = ooni.list_files(
-        first_date, last_date, test_type, country)
+    paginator = conn.get_paginator('list_objects_v2')
+    pages = paginator.paginate(Bucket=_BUCKET, Delimiter='/', Prefix=_PREFIX,
+                            StartAfter=f'{_PREFIX}{first_date.strftime("%Y%m%d")}')
 
-    def sync_file(entry: ooni_client.FileEntry):
-        target_file_path = _make_local_path(pathlib.Path('./media/ooni_data/'), entry)
-        if target_file_path.is_file():
-            return f'Skipped existing {entry.url.geturl()}]'
-        return fetch_file(entry, target_file_path)
-
-    def fetch_file(entry: ooni_client.FileEntry, target_file_path: pathlib.Path):
-        nonlocal num_measurements
-        os.makedirs(target_file_path.parent, exist_ok=True)
-        if ooni.cost_usd > cost_limit_usd:
-            raise CostLimitError(
-                f'Downloaded {ooni.bytes_downloaded / 2**20} MiB')
-        # We use a temporary file to atomatically write the destination and make sure we don't have partially written files.
-        # We put the temporary file in the same location as the destination because you can't atomically
-        # rename if they are in different devices, as is the case for Kaggle.
-        temp_path = target_file_path.with_name(f'{target_file_path.name}.tmp')
-        try:
-            with gzip.open(temp_path, mode='wt', encoding='utf-8', newline='\n') as target_file:
-                for measurement in entry.get_measurements():
-                    num_measurements += 1
-                    m = trim_measurement(measurement, max_string_size)
-                    ujson.dump(m, target_file)
-                    target_file.write('\n')
-                temp_path.replace(target_file_path)
-        except:
-            temp_path.unlink()
-            raise
-        return f'Downloaded {entry.url.geturl()} [{entry.size:,} bytes]'
-
-    with ThreadPool(processes=5 * os.cpu_count()) as sync_pool:
-        for msg in sync_pool.imap_unordered(sync_file, file_entries):
-            logging.info(msg)
-
-    logging.info(f'Measurements: {num_measurements}, Downloaded {ooni.bytes_downloaded/2**20:0.3f} MiB, Estimated Cost: ${ooni.cost_usd:02f}')
+    for page in pages:
+        for entry in page.get('CommonPrefixes', []):
+            date_dir = entry['Prefix']
+            date_str = posixpath.basename(posixpath.dirname(date_dir))
+            date = dt.datetime.strptime(date_str, "%Y%m%d").date()
+            if date >= last_date:
+                break
+            for hour in range(25):
+                prefix = f'''{date_dir}{hour:02}/{country}/'''
+                if test_type:
+                    prefix += f'{test_type}/'
+                for page in paginator.paginate(Bucket=page['Name'], Prefix=prefix):
+                    for entry in page.get('Contents', []):
+                        key = entry['Key']
+                        file_path = PosixPath(key)
+                        if file_path.name.endswith('.jsonl.gz'):
+                            print('Downloading Gzip file: ',file_path)
+                            file_test_type = file_path.parent.name
+                            url = SplitResult(
+                                        's3', page['Name'], key, None, None)
+                            conn.download_file(url.netloc, url.path, output_dir+url.path.replace(prefix, ''))
