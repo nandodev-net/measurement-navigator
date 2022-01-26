ed8eee623b0502fba328aed391aa94aef747ec17
jhondoes <jhondoes.does@gmail.com>
2022-01-26 01:47:45 -0500

    some fixes

--- vsf/apps/dashboard/utils.py
+++ vsf/apps/dashboard/utils.py
@@ -7,7 +7,7 @@ def utc_aware_date(date, sys_tz):
     aware_date = date
     if sys_tz == "2":
         if aware_date:
-            aware_date = date - timedelta(hours=4, minutes=28)
+            aware_date = date - timedelta(hours=4)
         else:
-            aware_date = datetime.now() - timedelta(hours=4, minutes=28)
+            aware_date = datetime.now() - timedelta(hours=4)
     return aware_date.astimezone()
