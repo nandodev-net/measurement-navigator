e7e17fe95396d29218d787726595aab6f17085a1
Patricia <patriciamacuto@gmail.com>
2022-02-14 15:40:01 +0100

    uploading new tasks for update and save stats from MLab, also, a few bugs were fixed and some API's were fixed

--- vsf/apps/api/public_stats_api/views.py
+++ vsf/apps/api/public_stats_api/views.py
@@ -1,8 +1,10 @@
 from django.http import Http404
 import datetime
+from decimal import Decimal
 import json
 import pytz
 from apps.main.public_stats.models import *
+from apps.main.asns.models          import ASN
 
 from rest_framework import status, generics
 from rest_framework.response import Response
@@ -52,3 +54,90 @@ class GetCategoryPublicStats(generics.GenericAPIView):
         stat = self.get_object(id)
         stat_json = CategoryPublicStatsDataSerializer(stat)
         return Response(stat_json.data, status=status.HTTP_200_OK)
+
+
+class StatsByASN(generics.GenericAPIView):
+    def get(self, request):
+        stats = AsnPublicStats.objects.all()
+        response = []
+        for stat in stats:
+            response.append({
+                'asn': stat.asn.name,
+                'blocked_domains': stat.blocked_domains_by_asn
+            })    
+        response_json = json.dumps(response)
+        return Response(json.loads(response_json), status=status.HTTP_200_OK)    
+        
+
+class StatsByCategory(generics.GenericAPIView):
+    def get(self, request):
+        stats = CategoryPublicStats.objects.all()    
+        response = []
+        for stat in stats:
+            response.append({
+                'category': stat.category.category_spa,
+                'blocked_domains': stat.blocked_domains_by_category
+            })    
+        response_json = json.dumps(response)
+        return Response(json.loads(response_json), status=status.HTTP_200_OK)    
+
+
+
+class SpeedInternetTimeline(generics.GenericAPIView):
+    serializer_class = SpeedInternetTimelineSerializer
+
+    
+    def get(self, request):
+        
+        last_day = SpeedInternet.objects.last().day
+
+        response = []
+        for i in range(0, 4):
+            date = last_day - datetime.timedelta( days = (i*7) )
+            speed_data = SpeedInternet.objects.filter(day = date).first()
+            
+            response.append({
+                'date': date.strftime("%b %d, %Y"),
+                'download_average': speed_data.download_average
+            })
+
+        response_json = SpeedInternetTimelineSerializer(response, many=True)
+
+        return Response(response_json.data, status=status.HTTP_200_OK)
+
+class SpeedInternetByISPTimeline(generics.GenericAPIView):
+    serializer_class = SpeedInternetByISPTimelineSerializer
+
+    
+    def get(self, request):
+        
+        last_day = SpeedInternetByISP.objects.last().day
+
+        response = []
+
+        
+        asns = ASN.objects.all()
+        for asn in asns:
+            
+            aux = []
+
+            for i in range(0, 4):
+                date = last_day - datetime.timedelta( days = (i*7) )
+                speed_data = SpeedInternetByISP.objects.filter(
+                    day = date,
+                    asn = asn
+                ).first()
+
+                aux.append({
+                    'date': date.strftime("%b %d, %Y"),
+                    'download_average': float(speed_data.download_average)
+                })
+
+            response.append({
+                asn.name: aux
+            })    
+            
+        
+
+        response_json = json.dumps(response)
+        return Response(json.loads(response_json), status=status.HTTP_200_OK)
