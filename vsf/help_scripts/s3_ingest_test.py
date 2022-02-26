ee43769a7071be45dfeca446e27c3f36e3daf142
Fernando Gonzalez <gonzalez0fer@gmail.com>
2022-02-26 02:11:34 -0500

    some prints deleted

--- vsf/help_scripts/s3_ingest_test.py
+++ vsf/help_scripts/s3_ingest_test.py
@@ -1,7 +1,8 @@
 from apps.api.fp_tables_api.s3_ingest import request_s3_meas_data
 import datetime
+import time
 
-ini_date_str = '01-06-2021'
+ini_date_str = '02-06-2021'
 end_date_str = '26-02-2022'
 
 test_types = ['webconnectivity']
@@ -10,8 +11,12 @@ first_date = datetime.datetime.strptime(ini_date_str, '%d-%m-%Y').date()
 last_date = datetime.datetime.strptime(end_date_str, '%d-%m-%Y').date()
 
 delta = datetime.timedelta(days=1)
-
+time_ini = time.time()
 while first_date <= last_date:
     print('Getting measurements from: ', first_date, ' to: ', first_date+delta)
     request_s3_meas_data(first_date=first_date, last_date=first_date+delta)
     first_date = first_date + delta
+
+time_end = time.time()
+print('\n\nS3 TOTAL ingest time: ', time_end-time_ini)
+print(first_date, ' ', last_date)
