e0562132e531812a5f42535f8cb8abc9c9c76c5d
jhondoes <jhondoes.does@gmail.com>
2022-02-25 23:28:46 -0500

    s3 download manager

--- vsf/help_scripts/s3_ingest_test.py
+++ vsf/help_scripts/s3_ingest_test.py
@@ -1,9 +1,17 @@
 from apps.api.fp_tables_api.s3_ingest import request_s3_meas_data
 import datetime
 
+ini_date_str = '01-06-2021'
+end_date_str = '26-02-2022'
+
 test_types = ['webconnectivity']
-first_date = (datetime.date.today() - datetime.timedelta(days=4)), 
-last_date = (datetime.date.today() - datetime.timedelta(days=3)),
 
+first_date = datetime.datetime.strptime(ini_date_str, '%d-%m-%Y').date()
+last_date = datetime.datetime.strptime(end_date_str, '%d-%m-%Y').date()
 
-request_s3_meas_data(test_types, first_date=first_date[0], last_date=last_date[0])
+delta = datetime.timedelta(days=1)
+
+while first_date <= last_date:
+    print('Getting measurements from: ', first_date, ' to: ', first_date+delta)
+    request_s3_meas_data(first_date=first_date[0], last_date=last_date[0])
+    first_date = first_date + delta
