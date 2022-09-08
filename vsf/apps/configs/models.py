27c9dcce2636aac383c5e7e336224316890f4729
Gibsy <gibsyvngr@gmail.com>
2022-09-08 15:46:58 -0400

    changed config model to be singleton

--- vsf/apps/configs/models.py
+++ vsf/apps/configs/models.py
@@ -1,7 +1,28 @@
+from distutils.command.config import config
 from django.db import models
 from model_utils.models import TimeStampedModel
 
 from django.core.exceptions import ValidationError
+
+
+
+class SingletonModel(models.Model):
+    """Singleton Django Model"""
+
+    class Meta:
+        abstract = True
+
+   
+
+    @classmethod
+    def load(cls):
+        """
+        Load object from the database. Failing that, create a new empty
+        (default) instance of the object and return it (without saving it
+        to the database).
+        """
+
+        
 
 class Config(TimeStampedModel):
 
@@ -24,6 +45,23 @@ class Config(TimeStampedModel):
         default=10,
         verbose_name = "How many measurements to check in each iteration of the algorithm"
     )
+
+    @classmethod
+    def get(cls):
+        """Get a model instance. Create one if no instance exists.
+        """
+        try:
+            return cls.objects.get()
+        except cls.DoesNotExist:
+            return cls()
+
+    def save(self, *args, **kwargs):
+        """
+        Save object to the database. Removes all other entries if there
+        are any.
+        """
+        self.__class__.objects.exclude(id=self.id).delete()
+        super(SingletonModel, self).save(*args, **kwargs)
 
     def clean(self):
             num_configs = Config.objects.all().count()
