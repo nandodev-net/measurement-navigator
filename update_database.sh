1a6922538b89f6f631d0c5a49e58829a156db76a
Gibsy <gibsyvngr@gmail.com>
2023-03-02 17:11:05 -0400

    updated script to use new version of docker compose

--- update_database.sh
+++ update_database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Use this script to run make migrations, migrate, load initial data (fixtures) and crete cache table
-docker-compose -f docker-compose.yml exec web python manage.py createcachetable
-docker-compose -f docker-compose.yml exec web python manage.py makemigrations 
-docker-compose -f docker-compose.yml exec web python manage.py migrate 
+docker compose -f docker-compose.yml exec web python manage.py createcachetable
+docker compose -f docker-compose.yml exec web python manage.py makemigrations 
+docker compose -f docker-compose.yml exec web python manage.py migrate 
